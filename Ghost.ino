f2532f4988c7580362b2269fa5bab8bddedb37fe
Lucas Mombach <39674074+Lucas-Mombach@users.noreply.github.com>
2022-12-19 14:11:09 -0300

    Ghost can change colors now

--- Ghost.ino
+++ Ghost.ino
@@ -1,22 +1,58 @@
 #include <Adafruit_NeoPixel.h>
-#define LED_PIN 6
+#define LED_PIN 3
 #define LED_COUNT 144
-#
+#define UP  5
+#define DOWN  6
 
 Adafruit_NeoPixel LED(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 int mov = 0;
 int aux = 0;
 bool eye = 0;
 
+//red         rgb(150,0,0)
+//            rgb(150,30,0) orange
+//    orange  rgb(150,100,0)
+//darkgreen   rgb(0,80,0)
+// lime       rgb(0,150,0)
+//limegreen   rgb(25,150,50)
+// violet     rgb(150,80,150)
+//fuchsia     rgb(150,0,150)
+//purple      rgb(80,0,80)
+int red[10] =   {150,150,150,0,0,25,150,150,80,80};
+int green[10]=  {0,30,100,80,150,150,80,0,0,0};
+int blue[10]=   {0,0,0,0,0,50,150,150,80,80};
+int n = 0;
+
+
 void setup()
 {
   LED.begin();
   LED.show();
+  pinMode(UP, INPUT);
+  pinMode(DOWN, INPUT);
   
 }
 
+
 void loop()
 {
+  if(digitalRead(UP) == HIGH) {
+  
+    n++;
+    if (n>9){
+      n=0;
+    }
+    
+  } 
+
+  if (digitalRead(DOWN) == HIGH) {
+    
+    n--;
+    if (n<0){
+      n=9;
+    }
+  }
+
   LED.clear();
   
   int olhos[4] = {52,55,67,64};
@@ -28,27 +64,27 @@ void loop()
   
   for(int i = 0; i < 32; i++) // corpo impar
   {
-    LED.setPixelColor(corpoImpar[i]-mov, 150, 0, 0);
+    LED.setPixelColor(corpoImpar[i]-mov, red[n], green[n], blue[n]);
     LED.show();
   }
 
     for(int i = 0; i < 25; i++) // corpo par
   {
-    LED.setPixelColor(corpoPar[i]+mov, 150, 0, 0);
+    LED.setPixelColor(corpoPar[i]+mov,  red[n], green[n], blue[n]);
     LED.show();
   }
 
   if(eye){
     for(int i = 0; i < 3; i++) // calda para esquerda
     {
-      LED.setPixelColor(caldaE[i]+mov, 150, 0, 0);
+      LED.setPixelColor(caldaE[i]+mov,  red[n], green[n], blue[n]);
       LED.show();
     }
   }
   else{
     for(int i = 0; i < 3; i++) // calda para direita
   {
-    LED.setPixelColor(caldaD[i]+mov, 150, 0, 0);
+    LED.setPixelColor(caldaD[i]+mov,  red[n], green[n], blue[n]);
     LED.show();
   }
   }
